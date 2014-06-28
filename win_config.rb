120174f0efcc09161eb679f761af1c59ba883aaf
atawmicpm <atawmicpm@gmail.com>
2014-06-28 02:50:21 -0700

    Update win_config.rb

--- win_config.rb
+++ win_config.rb
@@ -13,21 +13,21 @@ counter = 60
 
 puts "Waiting for #{FQDN} to come online..."
 
-until system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 'echo host up'"
+until system "knife winrm #{FQDN} -m -x Administrator -P 'password' -p 8888 'echo host up'"
         sleep 5
         exit 1 if counter == 0
         counter-=1
 end
 
-system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 \"wmic computersystem where name='%COMPUTERNAME%' call rename name='#{NAME}'\""
+system "knife winrm #{FQDN} -m -x Administrator -P 'password' -p 8888 \"wmic computersystem where name='%COMPUTERNAME%' call rename name='#{NAME}'\""
 
-system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 \"echo #{IP} #{FQDN} >> %SYSTEMDRIVE%\\Windows\\System32\\drivers\\etc\\hosts\""
+system "knife winrm #{FQDN} -m -x Administrator -P 'password' -p 8888 \"echo #{IP} #{FQDN} >> %SYSTEMDRIVE%\\Windows\\System32\\drivers\\etc\\hosts\""
 
-system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 \"reg add HKLM\\System\\currentcontrolset\\services\\tcpip\\parameters /v \\\"NV Domain\\\" /d #{DOMAIN} /f\""
+system "knife winrm #{FQDN} -m -x Administrator -P 'password' -p 8888 \"reg add HKLM\\System\\currentcontrolset\\services\\tcpip\\parameters /v \\\"NV Domain\\\" /d #{DOMAIN} /f\""
 
-system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 \"reg add HKLM\\System\\currentcontrolset\\services\\tcpip\\parameters /v Domain /d #{DOMAIN} /f\""
+system "knife winrm #{FQDN} -m -x Administrator -P 'password' -p 8888 \"reg add HKLM\\System\\currentcontrolset\\services\\tcpip\\parameters /v Domain /d #{DOMAIN} /f\""
 
-system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 \"shutdown /r /t 0\""
+system "knife winrm #{FQDN} -m -x Administrator -P 'password' -p 8888 \"shutdown /r /t 0\""
 
 
 #system "sleep 60"
@@ -38,7 +38,7 @@ system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 \"shutd
 
 
 
-#until system "knife winrm #{FQDN} -m -x Administrator -P 'cqU(FDe.yMN' -p 8888 'echo up'"
+#until system "knife winrm #{FQDN} -m -x Administrator -P 'password' -p 8888 'echo up'"
 #        sleep 5
 #        exit 1 if counter == 0
 #        counter-=1
